65a59748c0b27fdab4e91213744caac75edc9c1b
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-15 16:50:59 -0800

    Implemented logic for the Graph constructor, setDirection, addEdge, size getter, getLabel, and the getIndex helper

--- com/example/lab4/Graph.java
+++ com/example/lab4/Graph.java
@@ -11,29 +11,100 @@ public class Graph {
     private boolean isDirected;
     private int[][] adjacencyMatrix;
 
-    // Constructor
+    /**
+     * Constructor to initialize the Graph.
+     *
+     * @param vertexLabels - array of Strings that represent the names of the vertices
+     * @param isDirected   - indicates whether the graph is directed or not
+     */
     Graph(String[] vertexLabels, boolean isDirected) {
-
+        this.size = vertexLabels.length;
+        this.vertexLabels = vertexLabels;
+        this.isDirected = isDirected;
+        this.adjacencyMatrix = new int[size][size];
     }
 
-    // Setter
+    /**
+     * Can be used to manually set/reset whether the graph is directed or not.
+     *
+     * @param isDirected - indicates whether the graph is directed or not
+     */
     void setDirection(boolean isDirected) {
-
+        this.isDirected = isDirected;
     }
 
-    // Adds an edge between two vertices
-    void addEdge(String source, String destination) {
-
+    /**
+     * Returns true or false - true if the graph is a directed graph.
+     */
+    boolean isDirected() {
+        return isDirected;
     }
 
-    // returns the number of vertices
+    /**
+     * Adds an edge between vertex A to the vertex B.
+     * <p>
+     * If the graph is undirected this means setting
+     * two entries in the adjacency matrix to 1.
+     *
+     * @param A - the source vertex
+     * @param B - the destination vertex
+     */
+    void addEdge(String A, String B) {
+        // Get the index of the source and destination vertices
+        int sourceIndex = getIndex(A);
+        int destinationIndex = getIndex(B);
+
+        // Check if the vertices are valid
+        if (sourceIndex != 1 && destinationIndex != 1) {
+            adjacencyMatrix[sourceIndex][destinationIndex] = 1;
+            if (!isDirected) {
+                adjacencyMatrix[destinationIndex][sourceIndex] = 1;
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid vertices");
+        }
+    }
+
+    /**
+     * Helper method to get the index of a vertex label
+     *
+     * @param label - the vertex label
+     * @return - the index of the vertex label
+     */
+    private int getIndex(String label) {
+        for (int i = 0; i < size; i++) {
+            if (vertexLabels[i].equals(label)) {
+                return i;
+            }
+        }
+        return -1; // meaning the vertex label is not found!
+    }
+
+    /**
+     * Returns the number of vertices in the graph.
+     *
+     * @return - the number of vertices in the graph
+     */
     int size() {
-
+        return size;
     }
 
-    // returns the string name of the vertex number "v" internally
+    /**
+     * Returns the string name of the vertex number "v" internally.
+     * <p>
+     * (i.e. the vertex that was at position 'v' in the array that was passed to the constructor
+     * when the graph was initialized)
+     *
+     * @param v - an integer in the range 0 to N-1 inclusive.
+     * @return - the name of the vertex numbered 'v' on your internal list of vertices.
+     */
     String getLabel(int v) {
-
+        // Check if the vertex is valid
+        if (v >= 0 && v < size) {
+            return vertexLabels[v];
+        } else { // Otherwise, throw an exception
+            throw new IllegalArgumentException("Invalid vertex");
+        }
     }
 
     // returns a string representation of the adjacency matrix
