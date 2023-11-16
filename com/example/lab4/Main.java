1a6a6876c4f6ef9b06fa50415b42d61afbadd00a
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-15 18:18:09 -0800

    Added test graph (copied from lab pdf)

--- com/example/lab4/Main.java
+++ com/example/lab4/Main.java
@@ -1,10 +1,30 @@
 package com.example.lab4;
 
-// Name: Gathrean Dela Cruz
-// ID: A01167248
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Define vertices
+        String[] vertices = {"a", "b", "c", "d", "e", "f", "g", "h"};
+
+        // Create a graph
+        Graph G = new Graph(vertices, true); // Directed graph
+
+        // Add edges
+        G.addEdge("a", "b");
+        G.addEdge("a", "e");
+        G.addEdge("a", "f");
+        G.addEdge("b", "f");
+        G.addEdge("b", "g");
+        G.addEdge("c", "d");
+        G.addEdge("c", "g");
+        G.addEdge("d", "h");
+        G.addEdge("e", "f");
+        G.addEdge("g", "h");
+
+        // Perform DFS and BFS traversals
+        G.runDFS(false); // DFS traversal without printing
+
+        System.out.println("\n");
+
+        G.runBFS(false); // BFS traversal without printing
     }
 }
