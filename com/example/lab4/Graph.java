49f46432b0c787bb36fa6e22f2d5de44c3d11c1a
Gathrean Dela Cruz <gathrean@icloud.com>
2023-11-15 16:01:21 -0800

    Set up the required methods from the PDF's to-do list (currently empty)

--- com/example/lab4/Graph.java
+++ com/example/lab4/Graph.java
@@ -5,4 +5,73 @@ package com.example.lab4;
  */
 public class Graph {
 
+    // Constructor
+    Graph(String[] vertexLabels, boolean isDirected) {
+
+    }
+
+    // Setter
+    void setDirection(boolean isDirected) {
+
+    }
+
+    // Adds an edge between two vertices
+    void addEdge(String source, String destination) {
+
+    }
+
+    // returns the number of vertices
+    int size() {
+
+    }
+
+    // returns the string name of the vertex number "v" internally
+    String getLabel(int v) {
+
+    }
+
+    // returns a string representation of the adjacency matrix
+    String toString() {
+
+    }
+
+    // performs Depth First Search
+    void runDFS(boolean quiet) {
+
+    }
+
+    // performs Depth First Search at vertex v
+    void runDFS(String v, boolean quiet) {
+
+    }
+
+    // performs Breadth First Search
+    void runBFS(boolean quiet) {
+
+    }
+
+    // performs BFS starting at vertex v
+    void runBFS(String v, boolean quiet) {
+
+    }
+
+    // gets result of the most recently performed DFS
+    String getLastDFSOrder() {
+
+    }
+
+    // gets result of the most recently performed BFS
+    String getLastBFSOrder() {
+
+    }
+
+    // gets result of the most recently performed DFS
+    String getLastDFSDeadEndOrder() {
+
+    }
+
+    // gets result of the most recently performed BFS
+    String getLastBFSOrder() {
+
+    }
 }
